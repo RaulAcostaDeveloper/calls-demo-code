20c83fa1916dad92a36f9a97568df1e23f834bed
Raúl Acosta <raulacostadeveloper@outlook.com>
2024-11-07 19:43:01 -0600

    Hiding side bar on call details page and fixing header on top handling common resolutions.

--- components/layout/dynamic-layout.tsx
+++ components/layout/dynamic-layout.tsx
@@ -5,6 +5,8 @@ import Sidebar from "@/components/layout/sidebar";
 import { cn } from "@/lib/utils";
 import { useAuth } from "@/providers/auth-provider";
 import { LoaderIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 export default function DynamicLayout({
   children,
@@ -12,6 +14,18 @@ export default function DynamicLayout({
   children: React.ReactNode;
 }) {
   const { isAuthenticated, isLoading } = useAuth();
+  const [isCallDetailsPage, setIsCallDetailsPage] = useState(false);
+  const pathName = usePathname();
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (pathName.includes('/calls/call-detail')) {
+        setIsCallDetailsPage(true);
+      } else {
+        setIsCallDetailsPage(false);
+      }
+    }
+  }, [pathName]);
 
   if (isLoading) {
     return (
@@ -27,10 +41,10 @@ export default function DynamicLayout({
 
   return (
     <section className="block lg:flex">
-      <div>
+      <div className={`${isCallDetailsPage ? 'sidebarDetailsPage' : ''}`}>
         <Sidebar />
       </div>
-      <section className={cn("flex-1 my-10 mx-5 lg:my-5 lg:ml-64")}>
+      <section className={`${cn("flex-1 my-10 mx-5 lg:my-5 lg:ml-64")} ${isCallDetailsPage ? 'callDetailsPageLayout' : ''}`}>
         {children}
       </section>
     </section>
